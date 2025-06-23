ee3fd4571465053703ab387bbc67cda4be1a47fc
hongjinkim <ghdwls4654@naver.com>
2025-06-23 10:35:31 +0900

    [Add] audio, font asset

---
+++ practice/Assets/01_Scripts/FX/FXPoolManager.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class FXPoolManager : PoolManager<FXPoolManager, EFXPoolType>
+{
+    
+}
--- practice/Assets/01_Scripts/Utils/BasicSingleton.cs
+++ practice/Assets/01_Scripts/Utils/BasicSingleton.cs
@@ -1,4 +1,4 @@
-`using UnityEngine;
+using UnityEngine;
 
 public class BasicSingleton<T> : MonoBehaviour where T : MonoBehaviour
 {
